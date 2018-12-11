69b5b2282e983213e5201875cf4348ac11f7c4fc
Daniel Moura <daniel.lucio.moura@gmail.com>
2018-12-11 21:27:38 -0200

    Upload de fotos funcionando

--- app/src/main/java/com/ufscar/mobile/hestiaapp/cenarios/cadastra_imovel/CadastraImovelPresenter.kt
+++ app/src/main/java/com/ufscar/mobile/hestiaapp/cenarios/cadastra_imovel/CadastraImovelPresenter.kt
@@ -1,12 +1,25 @@
 package com.ufscar.mobile.hestiaapp.cenarios.cadastra_imovel
 
+import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
 import com.ufscar.mobile.hestiaapp.util.FirestoreImovelUtil
+import com.ufscar.mobile.hestiaapp.util.StorageUtil
 
 class CadastraImovelPresenter(val view: CadastraImovelContract.View): CadastraImovelContract.Presenter  {
+    override fun onInsertImovelWithPhoto(title: String, tipo: String, cidade: String, rua: String, numero: Int, complemento: String, referencia: String, max: Int, min: Int, preco: Int, quartos: Int, banheiros: Int, salas: Int, cozinhas: Int, vaga: Int, descricao: String, imageByte: ByteArray) {
+        view.showProgressBar()
+        val uidDono = FirebaseAuth.getInstance().currentUser?.uid
+        StorageUtil.uploadImovelPhoto(imageByte) {imagePath ->
+            FirestoreImovelUtil.insertImovel(title, tipo, cidade, rua, numero, complemento, referencia, max, min, preco, quartos, banheiros, salas, cozinhas, vaga, descricao, uidDono!!, imagePath)
+            view.hideProgressBar()
+            view.saveSuccess()
+        }
+    }
+
+
     override fun onInsertImovel(title: String, tipo: String, cidade: String, rua: String, numero: Int, complemento: String, referencia: String, max: Int, min: Int, preco: Int, quartos: Int, banheiros: Int, salas: Int, cozinhas: Int, vaga: Int, descricao: String) {
         val uidDono = FirebaseAuth.getInstance().currentUser?.uid
-        FirestoreImovelUtil.insertImovel(title, tipo, cidade, rua, numero, complemento, referencia, max, min, preco, quartos, banheiros, salas, cozinhas, vaga, descricao, uidDono!!)
+        FirestoreImovelUtil.insertImovel(title, tipo, cidade, rua, numero, complemento, referencia, max, min, preco, quartos, banheiros, salas, cozinhas, vaga, descricao, uidDono!!, null)
     }
 
 }
